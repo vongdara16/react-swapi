f541f0112552d2a0a0a338d53513d4913c6c3831
Justin Vongdara <vongdara16@gmail.com>
2022-03-17 11:53:37 -0400

    initial commit

---
+++ src/components/StarshipList.jsx
@@ -0,0 +1,14 @@
+// // import { useState } from 'react';
+
+
+// const StarshipList = (props) => {
+//   // const [starships, setStarships] = useState([])
+  
+//   return (  
+//     <>
+//       <h3>Starship List</h3>
+//     </>
+//   );
+// }
+ 
+// export default StarshipList;
